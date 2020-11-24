26a9b821715b3e4b59dacc4debfe4f12fdb4797a
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-11-24 15:27:51 +0100

    :card_file_box: Add Tweet model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,14 @@ class User < ApplicationRecord
 
   # L’email ne doit pas être nul, et doit être unique dans toute la base.
   validates :email, presence: true, uniqueness: true
+
+  # L’utilisateur a plusieurs tweets avec une colonne user_id dans la table Tweets.
+  has_many :tweets
 end
 
 # "azerty"
 # "password" => "anrsitdl86438rnauiten"
 # bcrypt("password") # => "anrsitdl86438rnauiten"
+
+user = User.last
+user.tweets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_104146) do
+ActiveRecord::Schema.define(version: 2020_11_24_141935) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_11_24_104146) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tweets", "users"
 end
